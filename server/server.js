353093b32fe66151148d0d48a3ec2a9c567809da
hildebrm <mbhildebrand6@gmail.com>
2025-04-06 10:05:43 -0400

    update dbpass

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ mongoose.connect(MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log("MongoDB connected"))
+  .then(() => console.log("MongoDB connected!"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
 app.get('/api/documents', async (req, res) => {
